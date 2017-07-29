1e737393f7c50988dd809afc72cdcd1f73005c73
Frobenius42 <adrien.marcenat@orange.fr>
2017-07-29 16:45:36 +0200

    add some graphism and camera behaviour

---
+++ Assets/Scripts/Camera2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2D : MonoBehaviour
+{
+	private Transform target;
+
+
+	void Start () 
+	{
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update () 
+	{
+		transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+	}
+}
