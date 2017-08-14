af51c852bc102668e0054ddc9c497142384581bf
Frobenius42 <adrien.marcenat@orange.fr>
2017-08-14 14:43:54 +0200

    change room system to improve romm transitions

---
+++ Assets/Scripts/Gameplay/RoomInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomInfo : MonoBehaviour 
+{
+	[SerializeField] float zoomFactor;
+	[SerializeField] Transform cameraNode;
+	private Camera2D camera;
+
+	void Awake()
+	{
+		camera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera2D>();
+	}
+
+	void OnEnable()
+	{
+		camera.SetTrackingTarget(cameraNode);
+		camera.SetZoom (zoomFactor);
+	}
+}
---
+++ Assets/Scripts/Gameplay/RoomSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSystem : MonoBehaviour 
+{
+	public static Transform currentRoom;
+
+	[SerializeField] GameObject exitRoom;
+	[SerializeField] GameObject enterRoom;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			currentRoom = enterRoom.transform;
+			enterRoom.SetActive (true);
+			exitRoom.SetActive (false);
+		}
+	}
+}
