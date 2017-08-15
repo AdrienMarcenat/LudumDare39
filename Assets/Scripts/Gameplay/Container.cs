0d1df1dbe3871a2c85f526b360196e199aa57409
Frobenius42 <adrien.marcenat@orange.fr>
2017-08-15 14:34:47 +0200

    container breaks when you shoot it, with a chance to spawn objetcs or monsters

---
+++ Assets/Scripts/Gameplay/Container.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Container : MonoBehaviour
+{
+	[SerializeField] GameObject[] spawnObjetcs;
+	[SerializeField] AudioClip breakingSound;
+	[SerializeField] Sprite brokenSprite;
+	[SerializeField] Vector3 spawningLocation;
+	[SerializeField] float spawningProbability;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Bullet") 
+		{
+			float spawningTest = Random.Range (0f, 1f);
+			if (spawningTest < spawningProbability && spawnObjetcs.Length != 0) {
+				int randomIndex = Random.Range (0, spawnObjetcs.Length);
+				GameObject spwanObject = Instantiate (spawnObjetcs [randomIndex], transform);
+				spwanObject.transform.localPosition = spawningLocation;
+			}
+
+			GetComponent<SpriteRenderer> ().sprite = brokenSprite;
+			SoundManager.PlayMultiple (breakingSound);
+			Destroy (this);
+		}
+	}
+}
+
