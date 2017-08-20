1d901b82bdf9871ae9b0e4bb7289e29cc16aa49e
Frobenius42 <adrien.marcenat@orange.fr>
2017-08-20 14:04:43 +0200

    add a FSM system, can be reused in other project

---
+++ Assets/Scripts/Engine/FSM.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FSM : MonoBehaviour
+{
+	public enum Action
+	{
+		Push,
+		Pop,
+		Clear,
+	};
+
+	private struct PendingChange
+	{
+		public Action action;
+		public int stateID;
+
+		public PendingChange(Action a, int ID = 0)
+		{
+			action = a;
+			stateID = ID;
+		}
+	};
+
+	private Stack<FSMState> stateStack;
+	private List<PendingChange> pendingList;
+	private Hashtable factories;
+
+
+	public FSM()
+	{
+		stateStack = new Stack<FSMState> ();
+		pendingList = new List<PendingChange> ();
+	}
+
+	public void RegisterState(int stateID, FSMState state)
+	{
+		factories.Add(stateID, state);
+	}
+
+	public void Update()
+	{
+		foreach(FSMState state in stateStack)
+		{
+			if (state.Update ())
+				break;
+		}
+		ApplyPendingChanges ();
+	}
+
+	public void PushState(int stateID)
+	{
+		pendingList.Add(new PendingChange(Action.Push, stateID));
+	}
+
+	public void PopState()
+	{
+		if (IsEmpty())
+			return;
+		pendingList.Add(new PendingChange(Action.Pop));
+	}
+
+	public void ClearStates()
+	{
+		pendingList.Add(new PendingChange(Action.Clear));
+	}
+
+	public bool IsEmpty()
+	{
+		return stateStack.Count == 0;
+	}
+
+	private FSMState FindState(int stateID)
+	{
+		return (FSMState) factories [stateID];
+	}
+
+	private void ApplyPendingChanges()
+	{
+		foreach (PendingChange change in pendingList)
+		{
+			switch (change.action)
+			{
+			case Action.Push:
+				stateStack.Push(FindState(change.stateID));
+				break;
+			case Action.Pop:
+				stateStack.Pop ();
+				break;
+			case Action.Clear:
+				stateStack.Clear ();
+				break;
+			}
+		}
+		pendingList.Clear();
+	}
+}
+
---
+++ Assets/Scripts/Engine/FSMState.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class FSMState : MonoBehaviour
+{
+	protected FSM fsm;
+	protected int ID;
+
+	public FSMState(FSM fsm)
+	{
+		this.fsm = fsm;
+		fsm.RegisterState (ID, this);
+	}
+
+	public abstract bool Update ();
+
+	protected void requestStackPush(int stateID)
+	{
+		fsm.PushState (stateID);
+	}
+
+	protected void requestStackPop()
+	{
+		fsm.PopState ();
+	}
+
+	protected void requestStateClear()
+	{
+		fsm.ClearStates ();
+	}
+}
+
