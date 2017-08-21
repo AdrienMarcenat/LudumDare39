8380b3a62921fb85c552bca22cdf8e9214f98a83
Frobenius42 <adrien.marcenat@orange.fr>
2017-08-21 11:59:06 +0200

    add a playerAnimator component

---
+++ Assets/Scripts/Gameplay/PlayerAnimator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimator : MonoBehaviour
+{
+	private Animator animator;
+	private PlayerEventManager playerEventManager;
+
+	void Awake ()
+	{
+		animator = GetComponent<Animator> ();
+		playerEventManager = GetComponent<PlayerEventManager> ();
+	}
+
+	void OnEnable()
+	{
+		playerEventManager.Move += MovePlayer;
+	}
+
+	void OnDisable()
+	{
+		playerEventManager.Move -= MovePlayer;
+	}
+
+	private void MovePlayer (float x, float y)
+	{
+		if (x != 0 || y != 0)
+			animator.SetBool ("PlayerMove", true);
+		else
+			animator.SetBool ("PlayerMove", false);
+	}
+}
+
--- Assets/Scripts/Gameplay/PlayerNormalState.cs
+++ Assets/Scripts/Gameplay/PlayerNormalState.cs
@@ -7,7 +7,6 @@ public class PlayerNormalState : FSMState
 	private PlayerEventManager playerEventManager;
 	private Health health;
 	private MovingObject body;
-	private Animator animator;
 
 	protected override void Awake()
 	{
@@ -18,7 +17,6 @@ public class PlayerNormalState : FSMState
 		health             = GetComponent<Health>();
 		body               = GetComponent<MovingObject> ();
 		weaponManager      = GetComponent<WeaponManager> ();
-		animator           = GetComponent<Animator> ();
 	}
 
 	public override void Enter ()
@@ -48,11 +46,6 @@ public class PlayerNormalState : FSMState
 	private void MovePlayer (float x, float y)
 	{
 		body.Move (x, y);
-
-		if (x != 0 || y != 0)
-			animator.SetBool ("PlayerMove", true);
-		else
-			animator.SetBool ("PlayerMove", false);
 
 		Vector3 direction = (Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position).normalized;
 		Quaternion rotation = Quaternion.Euler (0, 0, Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg + 90);
